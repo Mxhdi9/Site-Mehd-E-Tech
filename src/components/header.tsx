f139ad8419a59b9dba0baf3f375b1a848b5075c0
Mxhdi9 <xd2fusionyt@gmail.Com>
2023-06-08 17:37:39 +0200

    SALAM

---
+++ src/components/header.tsx
@@ -0,0 +1,5 @@
+export const Header:React.FC = () => {
+    return (
+        console.log('TA MERE')
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Index(){
+  return(
+<div></div>
+  )
+}
+export default Index;
