d39afb1aac4b506317c39a65e263cd25c95b25d9
Mxhdi9 <xd2fusionyt@gmail.Com>
2023-06-08 17:42:15 +0200

    Salam

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ContactPage from './ContactPage';
+
 
 function HomePage() {
   return (
     <div>
       <h1>Accueil</h1>
       <p>Bienvenue sur notre site e-commerce Informatique.</p>
-      <ContactPage />
     </div>
   );
 }
