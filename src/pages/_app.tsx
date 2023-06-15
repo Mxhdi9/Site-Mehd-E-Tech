ff39999712de51780d9f21e3378156f030c2470b
Mxhdi9 <xd2fusionyt@gmail.Com>
2023-06-15 10:50:27 +0200

    Création d'une page de login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '@/components/header'
 export default function App({ Component, pageProps }: AppProps) {
-  return <div>
+  return <div className='bg-black'>
     <Header/>
   <Component {...pageProps} /> </div>
 }
