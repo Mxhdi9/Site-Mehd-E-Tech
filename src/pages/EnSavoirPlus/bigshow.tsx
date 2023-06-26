278e10ff712bfec61b07ee178242e0325bd8dedc
Mxhdi9 <xd2fusionyt@gmail.Com>
2023-06-26 11:02:04 +0200

    Création des pages "EnSavoirPlus" à la place de "Ajouter Au Panier" et liaison de ces pages dans le "index"

---
+++ src/pages/EnSavoirPlus/bigshow.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+function BigShow() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 p-8">
+        {/* Affichage du produit à gauche */}
+        <div className="rounded-lg overflow-hidden">
+          <Image
+            src="/img/pcgaming.jpg"
+            alt="Produit"
+            className="w-full"
+            width={800}
+            height={600}
+          />
+        </div>
+      </div>
+      <div className="w-1/2 p-8">
+        {/* Nom du produit en haut de la page */}
+        <h1 className="text-white text-4xl font-bold mb-8">Pc Gaming BIG SHOW</h1>
+
+        {/* Description de produit à droite */}
+        <p className="text-white text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut
+          lacus ex. Curabitur vel bibendum magna, ut efficitur neque. Sed
+          tristique diam lectus, nec bibendum nunc lacinia sed. Pellentesque
+          habitant morbi tristique senectus et netus et malesuada fames ac
+          turpis egestas. Quisque hendrerit purus nec ligula facilisis, eget
+          dignissim dolor dignissim. Etiam venenatis lobortis semper. Fusce
+          iaculis quam in fringilla lacinia. Nullam et mauris eu lacus blandit
+          pharetra. Aliquam et neque vulputate, pretium leo vel, ultrices diam.
+          Proin eget sapien aliquet, malesuada neque eget, accumsan odio. Duis
+          dapibus, risus et venenatis lacinia, ipsum mi congue metus, at
+          pellentesque nisl nunc id felis. Suspendisse non varius ex. Duis
+          commodo feugiat augue, eget ultrices lectus ultricies vel. Fusce
+          sollicitudin condimentum quam id malesuada. Vestibulum in ante nec
+          ipsum rutrum consectetur.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default BigShow;
